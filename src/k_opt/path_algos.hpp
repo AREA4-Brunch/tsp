f7a3519d4cf247f86af55f0a8cec57b4dd14776c
Sale <sale.radenkovic@gmail.com>
2025-12-24 21:14:50 +0100

    Removed debugging code.

--- src/k_opt/path_algos.hpp
+++ src/k_opt/path_algos.hpp
@@ -23,18 +23,6 @@ inline typename v_t::traits::node_ptr get_neighbour(
     }
 }
 
-template<IntrusiveVertex v_t>
-[[ gnu::always_inline, gnu::hot ]]
-inline typename v_t::traits::node_ptr get_neighbour(
-    typename v_t::traits::const_node_ptr const ptr,
-    typename v_t::traits::const_node_ptr const opp,
-    const bool is_next_hint
-) noexcept {
-    return is_next_hint
-        ? get_neighbour<true, v_t>(ptr, opp)
-        : get_neighbour<false, v_t>(ptr, opp);
-}
-
 template<IntrusiveVertex v_t, bool is_next_hint = true>
 [[ gnu::always_inline, gnu::hot ]]
 inline void set_neighbour(
@@ -79,39 +67,11 @@ inline void reverse(
     typename v_t::traits::node_ptr end,
     typename v_t::traits::node_ptr after_end
 ) noexcept {
-    if (start == end) [[ unlikely ]] return;
     set_neighbour<v_t, false>(before_start, start, end);
     set_neighbour<v_t, true>(end, after_end, before_start);
     set_neighbour<v_t, true>(start, before_start, after_end);
     set_neighbour<v_t, false>(after_end, end, start);
 }
-
-// template<IntrusiveVertex v_t>
-// inline void swap_sequential_segs(
-//     typename v_t::traits::node_ptr prev_s1,
-//     typename v_t::traits::node_ptr s1_start,
-//     typename v_t::traits::node_ptr s1_end,
-//     typename v_t::traits::node_ptr s2_start,
-//     typename v_t::traits::node_ptr s2_end,
-//     typename v_t::traits::node_ptr after_s2
-// ) noexcept {
-//     set_neighbour<v_t, true>(prev_s1, s1_start, s2_start);
-//     if (s2_start != s2_end) [[ likely ]] {
-//         set_neighbour<v_t, false>(s2_start, s1_end, prev_s1);
-//         set_neighbour<v_t, false>(s2_end, after_s2, s1_start);
-//     } else [[ unlikely ]] {
-//         v_t::traits::set_previous(s2_start, prev_s1);
-//         v_t::traits::set_next(s2_start, s1_start);
-//     }
-//     if (s1_start != s1_end) [[ likely ]] {
-//         set_neighbour<v_t, false>(s1_start, prev_s1, s2_end);
-//         set_neighbour<v_t, false>(s1_end, s2_start, after_s2);
-//     } else [[ unlikely ]] {
-//         v_t::traits::set_previous(s1_start, s2_end);
-//         v_t::traits::set_next(s1_start, after_s2);
-//     }
-//     set_neighbour<v_t, false>(after_s2, s2_end, s1_end);
-// }
 
 template<IntrusiveVertex v_t>
 [[ gnu::always_inline, gnu::hot ]]
